07c90d847f1d6cde8bf01292d890c8f841c41856
MiJanuL <skmijanulh@gmail.com>
2022-08-07 18:04:57 +0530

    82. Extracting Data on the Catch-All Page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,8 +1,45 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventList from "../../components/events/event-list";
+import { getFilteredEvents } from "../../dummy-data";
+
 function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const [year, month] = filterData;
+  const numYear = +year;
+  const numMonth = +month;
+
+  if (
+    isNaN(numMonth) ||
+    isNaN(numYear) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filterData || filterData.length == 0) {
+    return <p>No events found for the chosen filter!</p>;
+  }
+
   return (
-    <div>
-      <h1>Filtered Events</h1>
-    </div>
+    <Fragment>
+      <EventList items={filteredEvents} />
+    </Fragment>
   );
 }
 
